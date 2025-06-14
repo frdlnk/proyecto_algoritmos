2e602b0d2d390dea4b1397205a829f8fcf081e81
Freddy <nglimaai01@gmail.com>
2025-06-14 17:10:10 -0600

    inital commit

---
+++ src/proyecto_algoritmos/Proyecto_algoritmos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_algoritmos;
+
+/**
+ *
+ * @author Gigabyte
+ */
+public class Proyecto_algoritmos {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
